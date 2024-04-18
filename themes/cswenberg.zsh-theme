88819067982b5230a1d7f83be7310972bf739e0e
Conner Swenberg <cls364@cornell.edu>
2024-04-18 13:05:36 -0400

    Change primary color purple->green

--- themes/cswenberg.zsh-theme
+++ themes/cswenberg.zsh-theme
@@ -1,5 +1,5 @@
 # cswenberg.zsh-theme
 
-PROMPT='%{$FG[135]%}%c %{$FG[208]%}»%{$reset_color%} '
+PROMPT='%{$fg[green]%}%c %{$FG[208]%}»%{$reset_color%} '
 RPROMPT='%{$FG[008]%}$(git_current_branch)%{$reset_color%}'
 
--- web-aliases.zsh
+++ web-aliases.zsh
@@ -8,5 +8,10 @@ alias gen="npx prisma generate"
 alias seed="npx prisma db seed"
 alias reset="blitz prisma migrate reset && gen"
 alias nb="npm run build"
+alias nd="npm run dev"
 alias md="mintlify dev"
 alias mlatest="sudo npm i -g mintlify@latest"
+
+# Backend
+alias cb="cargo build"
+alias cr="cargo run"
